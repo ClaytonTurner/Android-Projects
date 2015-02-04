431975d75a9b288b6aeff928d911671bc67ec0e4
Clayton Turner <caturner3@g.cofc.edu>
2015-02-04 13:45:54 -0500

    shaking the phone clears the counter

--- ButtonCounter/app/src/main/java/com/android/claytonturner/buttoncounter/MainActivity.java
+++ ButtonCounter/app/src/main/java/com/android/claytonturner/buttoncounter/MainActivity.java
@@ -1,5 +1,9 @@
 package com.android.claytonturner.buttoncounter;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +13,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SensorEventListener{
     private int counter;
+    //sensor detection
+    //adapted from: http://androidcodeexamples.blogspot.com/2011/12/shake-listener-demoexample.html
+    private SensorManager sensor_manager;
+    private long lastUpdate;
+    Boolean bool = false, bool1 = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //sensor
+        sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor_manager.registerListener(this,sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdate = System.currentTimeMillis();
+
+        //typical behavior
         Button count_button = (Button) findViewById(R.id.count_button);
         Button clear_button = (Button) findViewById(R.id.clear_button);
         final TextView text = (TextView) findViewById(R.id.textView);
@@ -42,14 +58,59 @@ public class MainActivity extends ActionBarActivity {
 
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
-        outState.putInt("count",counter);
+        outState.putInt("count", counter);
     }
 
-    /*protected void onRestoreInstanceState(Bundle inState){
-        super.onRestoreInstanceState(inState);
-        counter = inState.getInt("count");
-        text.setText(String.valueOf(counter));
-    }*/
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        final TextView text = (TextView) findViewById(R.id.textView);
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float[] values = event.values;
+            // Movement
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            float accsqr = (x * x + y * y + z * z)
+                    / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+            //debug statement
+            //System.out.println("accsqr is:- " + accsqr);
+            long actualTime = System.currentTimeMillis();
+            if (accsqr >= 5) //
+            {
+                bool1 = false;
+                if (bool == false) {
+                    if (actualTime - lastUpdate < 1000) {
+                        return;
+                    }
+                    lastUpdate = actualTime;
+                    //Here Define Method for OnStartShaking
+                    counter = 0;
+                    text.setText(String.valueOf(counter));
+                    bool = true;
+                }
+            } else {
+                bool = false;
+                if (bool1 == false) {
+                    if (actualTime - lastUpdate < 4000) {
+                        return;
+                    }
+                    lastUpdate = actualTime;
+                    //Here Define Method for OnStopShaking
+
+                    bool1 = true;
+                }
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+
+    }
 
 
     @Override
