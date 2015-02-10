0231fccdb1a9c99f3e7569196ce8102b49b7fb2b
Clayton Turner <caturner3@g.cofc.edu>
2015-02-10 09:23:17 -0500

    Portrait layout for calculator setup

---
+++ Calculator/app/src/main/java/com/android/claytonturner/calculator/Operation.java
@@ -0,0 +1,12 @@
+package com.android.claytonturner.calculator;
+
+/**
+ * Created by Clayton on 2/10/2015.
+ */
+/**
+ *  The set of operations that can be performed by the calculator.
+ */
+public enum Operation
+{
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUALS, CLEAR, CLEAR_ENTRY;
+}
