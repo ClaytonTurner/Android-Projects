5d29f511f87ba14a75c9e6853594fb64faa4f64a
Clayton Turner <caturner3@g.cofc.edu>
2015-03-02 17:04:36 -0500

    Tip calculator functional

--- TipCalculator/app/src/main/java/com/android/claytonturner/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/android/claytonturner/tipcalculator/MainActivity.java
@@ -2,8 +2,17 @@ package com.android.claytonturner.tipcalculator;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +21,46 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Boolean tipSelected = false;
+        final Boolean billEntered = false;
+        //int tipOptions = 4;
+
+        final EditText bill = (EditText) findViewById(R.id.billAmount);
+        final RadioGroup tipRadioGroup = (RadioGroup) findViewById(R.id.tipGroup);
+
+        final TextView tipTotal = (TextView) findViewById(R.id.tipCount_result);
+        final TextView totalPrice = (TextView) findViewById(R.id.totalPrice_result);
+        bill.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER &&
+                         !bill.getText().toString().contains("[a-zA-z]"))) {
+                    // Perform action on key press
+                        double tipPercentage;
+                        switch (((RadioGroup)findViewById(R.id.tipGroup)).getCheckedRadioButtonId()){
+                            case R.id.ten: tipPercentage = .1;
+                                break;
+                            case R.id.fifteen: tipPercentage = .15;
+                                break;
+                            case R.id.eighteen:    tipPercentage = .18;
+                                break;
+                            case R.id.twenty:  tipPercentage = .2;
+                                break;
+                            default:    tipPercentage = .2; // Shouldn't happen, but let's assume generosity
+                                break;
+                        }
+                       NumberFormat numformat = NumberFormat.getCurrencyInstance(Locale.US);
+                       Double enteredBill = Double.parseDouble(bill.getText().toString());
+                       Double tip = enteredBill * tipPercentage;
+                       Double price = enteredBill + (enteredBill * tipPercentage);
+                       tipTotal.setText(numformat.format(tip));
+                       totalPrice.setText(numformat.format(price));
+                       return true;
+                }
+                return false;
+            }
+        });
     }
 
 
