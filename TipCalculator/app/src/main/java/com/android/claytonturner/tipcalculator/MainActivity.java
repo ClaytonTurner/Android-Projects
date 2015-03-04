992e7002a16c6aa9a7fd3b9dad23b2ae5cce21d0
Clayton Turner <caturner3@g.cofc.edu>
2015-03-04 13:45:06 -0500

    Add serializability and refactored getValues into its own function

--- TipCalculator/app/src/main/java/com/android/claytonturner/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/android/claytonturner/tipcalculator/MainActivity.java
@@ -21,15 +21,37 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Boolean tipSelected = false;
-        final Boolean billEntered = false;
-        //int tipOptions = 4;
 
         final EditText bill = (EditText) findViewById(R.id.billAmount);
         final RadioGroup tipRadioGroup = (RadioGroup) findViewById(R.id.tipGroup);
 
         final TextView tipTotal = (TextView) findViewById(R.id.tipCount_result);
         final TextView totalPrice = (TextView) findViewById(R.id.totalPrice_result);
+        if(savedInstanceState != null){
+            tipTotal.setText(savedInstanceState.getString("tip"));
+            totalPrice.setText(savedInstanceState.getString("total"));
+        }
+        tipRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+                double tipPercentage;
+                switch (((RadioGroup)findViewById(R.id.tipGroup)).getCheckedRadioButtonId()){
+                    case R.id.ten: tipPercentage = .1;
+                        break;
+                    case R.id.fifteen: tipPercentage = .15;
+                        break;
+                    case R.id.eighteen:    tipPercentage = .18;
+                        break;
+                    case R.id.twenty:  tipPercentage = .2;
+                        break;
+                    default:    tipPercentage = .2; // Shouldn't happen, but let's assume generosity
+                        break;
+                }
+                setValues(tipPercentage);
+            }
+        });
         bill.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event){
                 // If the event is a key-down event on the "enter" button
@@ -37,30 +59,44 @@ public class MainActivity extends ActionBarActivity {
                         (keyCode == KeyEvent.KEYCODE_ENTER &&
                          !bill.getText().toString().contains("[a-zA-z]"))) {
                     // Perform action on key press
-                        double tipPercentage;
-                        switch (((RadioGroup)findViewById(R.id.tipGroup)).getCheckedRadioButtonId()){
-                            case R.id.ten: tipPercentage = .1;
-                                break;
-                            case R.id.fifteen: tipPercentage = .15;
-                                break;
-                            case R.id.eighteen:    tipPercentage = .18;
-                                break;
-                            case R.id.twenty:  tipPercentage = .2;
-                                break;
-                            default:    tipPercentage = .2; // Shouldn't happen, but let's assume generosity
-                                break;
+                        if(bill.getText().toString().matches(".*\\d.*")) {
+                            double tipPercentage;
+                            switch (((RadioGroup) findViewById(R.id.tipGroup)).getCheckedRadioButtonId()) {
+                                case R.id.ten:
+                                    tipPercentage = .1;
+                                    //setValues(tipPercentage);
+                                    break;
+                                case R.id.fifteen:
+                                    tipPercentage = .15;
+                                    //setValues(tipPercentage);
+                                    break;
+                                case R.id.eighteen:
+                                    tipPercentage = .18;
+                                    //setValues(tipPercentage);
+                                    break;
+                                case R.id.twenty:
+                                    tipPercentage = .2;
+                                    //setValues(tipPercentage);
+                                    break;
+                                default:
+                                    tipPercentage = .2; // Shouldn't happen, but let's assume generosity
+                                    //setValues(tipPercentage);
+                                    break;
+                            }
+                            setValues(tipPercentage);
                         }
-                       NumberFormat numformat = NumberFormat.getCurrencyInstance(Locale.US);
-                       Double enteredBill = Double.parseDouble(bill.getText().toString());
-                       Double tip = enteredBill * tipPercentage;
-                       Double price = enteredBill + (enteredBill * tipPercentage);
-                       tipTotal.setText(numformat.format(tip));
-                       totalPrice.setText(numformat.format(price));
                        return true;
                 }
                 return false;
             }
         });
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        // Can use .toString() or cast to String - both work
+        outState.putString("tip",((TextView)findViewById(R.id.tipCount_result)).getText().toString());
+        outState.putString("total",((TextView)findViewById(R.id.totalPrice_result)).getText().toString());
     }
 
 
@@ -84,5 +120,19 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setValues(Double tipPercentage){
+        final EditText bill = (EditText) findViewById(R.id.billAmount);
+        final TextView tipTotal = (TextView) findViewById(R.id.tipCount_result);
+        final TextView totalPrice = (TextView) findViewById(R.id.totalPrice_result);
+        if(!bill.getText().equals("") && bill.getText().toString().matches(".*\\d.*")) {
+            NumberFormat numformat = NumberFormat.getCurrencyInstance(Locale.US);
+            Double enteredBill = Double.parseDouble(bill.getText().toString());
+            Double tip = enteredBill * tipPercentage;
+            Double price = enteredBill + (enteredBill * tipPercentage);
+            tipTotal.setText(numformat.format(tip));
+            totalPrice.setText(numformat.format(price));
+        }
     }
 }
